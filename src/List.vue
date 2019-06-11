4e6999bf90f0d78ec1afd2b2862838a9fce9a018
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-06-11 14:40:35 -0300

    Add files via upload

---
+++ src/List.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Filters & Mixins</h1>
+    <input v-model="filterText">
+    <ul>
+      <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { fruitMixin } from './fruitMixin';
+
+export default {
+    mixins: [fruitMixin]
+};
+</script>
+
+<style>
+</style>
